fd78235867a0f240237f5a6345151cc26fc7adec
Sahoo <souravsahoo@deloitte.com>
2020-09-14 21:33:48 +0530

    Updates

---
+++ precache-manifest.473f9a4eef56f4cb284f1a091197cf8c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20628fd94c507b791f53a9ab945fdd47",
+    "url": "/Recipe-App/index.html"
+  },
+  {
+    "revision": "47f067047d812bf59c6e",
+    "url": "/Recipe-App/static/css/main.8c49f275.chunk.css"
+  },
+  {
+    "revision": "6d67894d5ab4d3145ad6",
+    "url": "/Recipe-App/static/js/2.20dedf64.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Recipe-App/static/js/2.20dedf64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47f067047d812bf59c6e",
+    "url": "/Recipe-App/static/js/main.7430f361.chunk.js"
+  },
+  {
+    "revision": "c035ec1896e8fb26f1a6",
+    "url": "/Recipe-App/static/js/runtime-main.5105d1b0.js"
+  }
+]);
